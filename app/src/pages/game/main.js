9e5de14bcbcfec38cb7e0b8039046e3610080d15
Yossi Kolesnicov <yokolesn@microsoft.com>
2018-06-27 15:09:46 +0300

    Ball angle changes

--- app/src/pages/game/main.js
+++ app/src/pages/game/main.js
@@ -1,7 +1,7 @@
 import {State} from "./models/state.js";
 
 const INITIAL_BALL_SPEED = 1;
-
+const MATKA_BALL_ANGLE_INCREASE_RANGE = 2;
 
 class Pong{
     constructor(){
@@ -31,6 +31,26 @@ class Pong{
 
     moveMatka(matka, isUp){
         this.state.matkot[matka].nextPosition(isUp);
+    }
+
+    /**
+     * Called when the ball should change angle
+     * @param isMatka Whether the ball hit the matka. If false, the ball hit one of the horizontal walls
+     */
+    setBallAngle(isMatka){
+        if (isMatka)
+            this.state.ball.angle = this.getBallNewAngleOnMatkaHit();
+        else
+            this.state.ball.angle = 180 - this.state.ball.angle;
+    }
+
+    private getBallNewAngleOnMatkaHit(){
+        const matka = this.state.matkot[this.state.ball.speed > 0 ? 1 : 0];
+        const ballOffsetFromMatkaCenter = Math.abs(matka.yPosition + matka.size / 2 - this.state.ball.position.y);
+        const hitRatio = ballOffsetFromMatkaCenter / matka.size;
+        const newAngle = this.state.ball.angle * (1.5 * hitRatio - 0.5);
+
+        return newAngle * -1;
     }
 }
 
