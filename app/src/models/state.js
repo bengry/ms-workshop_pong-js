dcf6b18b435025c3f809f9a546336bff941c4454
Yossi Kolesnicov <yokolesn@microsoft.com>
2018-06-27 11:39:36 +0300

    Added matka position

--- app/src/models/state.js
+++ app/src/models/state.js
@@ -3,12 +3,14 @@ import {Score} from "./score.js";
 import {Matka} from "./matka.js";
 import {Player} from "./player.js";
 
+const MATKA_SPEED = 5;
+
 export class State{
     constructor(){
         const randAngle = (Math.random() < 0.5 ? 1 : -1) * (60 + Math.random() * 60);
         this.ball = new Ball(0, [0.5,0.5], randAngle);
         this.score = new Score();
-        this.matkot = [new Matka(), new Matka()];
+        this.matkot = [new Matka(MATKA_SPEED), new Matka(MATKA_SPEED)];
         this.players = [new Player('Tal'), new Player('Yossi')];
 
         console.log("INIT state", this);
