f89566054273fa724a65304834d45750c88d24d1
Ben Grynhaus <bengr@microsoft.com>
2018-06-27 11:32:31 +0300

    restore last player names

--- app/src/pages/intro/intro.js
+++ app/src/pages/intro/intro.js
@@ -20,7 +20,25 @@ export class Intro {
   }
 }
 
-const intro = new Intro({
+const readLastState = () => {
+  const lastPlayers = state.players.get();
+  if (!lastPlayers) {
+    return [];
+  }
+
+  return lastPlayers.map(p => p.name);
+};
+
+const player1InputEl = document.querySelector('#Intro__Player1__input');
+const player2InputEl = document.querySelector('#Intro__Player2__input');
+
+const lastPlayers = readLastState();
+if (lastPlayers) {
+  player1InputEl.value = lastPlayers[0];
+  player2InputEl.value = lastPlayers[1];
+}
+
+new Intro({
   playerInputs: [
     document.querySelector('#Intro__Player1__input'),
     document.querySelector('#Intro__Player2__input'),
