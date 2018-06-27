5dbfe31e8dfcebc3effdf1ee3ffc58a1198c06cc
Shai Rose <shai.rose@microsoft.com>
2018-06-27 15:11:26 +0300

    style

--- app/src/pages/game/renderer.js
+++ app/src/pages/game/renderer.js
@@ -1,18 +1,20 @@
 export class Renderer {
     constructor(state) {
-        this.lastState = state;
+        this.lastState = {...state};
 
+        this.boardEl = document.getElementById('board');
         this.ballEl = document.getElementById('ball');
         this.matka1El = document.getElementById('matka1');
         this.matka2El = document.getElementById('matka2');
+
+        this.boardSize = [this.boardEl.offsetWidth, this.boardEl.offsetHeight];
     }
 
     paint(state) {
+        this.ballEl.style.left = (state.ball.position[0] * this.boardSize[0]) + 'px';
+        this.ballEl.style.top = (state.ball.position[1] * this.boardSize[1]) + 'px';
 
-        this.ballEl.style.left = state.ball.position[0] + 'px';
-        this.ballEl.style.top = state.ball.position[1] + 'px';
-
-        this.lastState = state;
+        this.lastState = {...state};
     }
 }
 
